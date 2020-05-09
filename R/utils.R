b21331198a165cc02853ac1dd77679fc865d0264
wguo-research <abg8899@outlook.com>
2020-05-09 19:41:54 +0800

    debug

--- R/utils.R
+++ R/utils.R
@@ -495,11 +495,12 @@ getMouseGene <- function(hg.genes, bool.name = F, deduplicate = T){
 #' The row names are the features' anmes. The columns are patients' labels.
 #' @param surv.time The survival time of patients. It should be in accord with the columns of data.
 #' @param surv.event The status indicator of patients. 0=alive, 1=dead. It should be in accord with the columns of data.
-#' @param cut.off
+#' @param cut.off The percentage threshold to divide patients into two groups.
+#' The default is 0.5, which means the patients are divided by median.
 #' @param savePath The path to save the survival plots of genes or signatures (the default is NULL and the plots will be return without saving).
 #'
 #'
-#' @return
+#' @return A list of survival curves plots.
 #' @export
 #'
 #' @import survival survminer
