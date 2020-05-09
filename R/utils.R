db0ab2f9e635dc72d6f2046377f011dc00e9a352
wguo-research <abg8899@outlook.com>
2020-05-09 19:52:21 +0800

    debug

--- R/utils.R
+++ R/utils.R
@@ -497,6 +497,8 @@ getMouseGene <- function(hg.genes, bool.name = F, deduplicate = T){
 #' @param surv.event The status indicator of patients. 0=alive, 1=dead. It should be in accord with the columns of data.
 #' @param cut.off The percentage threshold to divide patients into two groups.
 #' The default is 0.5, which means the patients are divided by median.
+#' Other values, such as 0.4, means the first 40 percent patients are set "Low" group
+#' and the last 40 percent are set "High" group (the median 20 percent are discarded).
 #' @param savePath The path to save the survival plots of genes or signatures (the default is NULL and the plots will be return without saving).
 #'
 #'
